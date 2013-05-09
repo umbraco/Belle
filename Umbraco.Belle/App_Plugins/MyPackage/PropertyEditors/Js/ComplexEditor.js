d420b38c9ccaba1ce3e33e25967ab76fd038dc1d
Shannon Deminick <sdeminick@gmail.com>
2013-05-09 15:53:32 +1000

    updated property editors POC, have namespaces working have angular views rendered via MVC have angular calling web api to save data.

---
+++ Umbraco.Belle/App_Plugins/MyPackage/PropertyEditors/Js/ComplexEditor.js
@@ -0,0 +1,40 @@
+﻿function ComplexEditorController($scope, $http) {
+
+    $scope.properties = [
+        {
+            alias: "numbers",
+            label: "Numeric",
+            description: "Enter a numeric value",
+            view: "/App_Plugins/MyPackage/PropertyEditors/numeric.html",
+            value: "12345987765",
+            config: {
+                format: "## #### ####"
+            }
+        },
+        {
+            alias: "serverEnvironment",
+            label: "Server Info",
+            description: "Some server information",
+            view: "@(serverEnvironmentView)",
+            value: ""
+        }
+    ];
+
+    $scope.save = function () {
+        var valueToPost = [];
+        for (var p in $scope.properties) {
+            valueToPost.push({
+                alias: $scope.properties[p].alias,
+                value: $scope.properties[p].value
+            });
+        }
+        $http.post('@(postSaveUrl)', valueToPost).
+            success(function (data, status, headers, config) {
+                alert("success!");
+            }).
+            error(function (data, status, headers, config) {
+                alert("failed!");
+            });
+    };
+
+}
---
+++ Umbraco.Belle/Controllers/ContentEditorApiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace Umbraco.Belle.Controllers
+{
+    [PluginController("UmbracoEditors")]
+    public class ContentEditorApiController : UmbracoApiController
+    {
+        /// <summary>
+        /// Saves content
+        /// </summary>
+        /// <returns></returns>
+        public HttpResponseMessage PostSaveContent(JArray values)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "success!");
+        }
+
+    }
+}
