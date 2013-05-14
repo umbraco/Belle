0db6474c40723ccd8ab4d46785be577e9e5a514c
Per Ploug Krogslund <pph@umbraco.dk>
2013-05-14 09:10:07 +0200

    Ignored node_modules to get rid of insane filenames in git

--- Umbraco.Belle.Client/src/app/common/dialogs/mediapickercontroller.js
+++
@@ -1,4 +0,0 @@
-//used for the media picker dialog
-app.controller("mediaPickerDialogController", function ($scope, mediaFactory) {	
-	$scope.images = mediaFactory.rootMedia();
-});
--- Umbraco.Belle.Client/src/app/content/createController.js
+++
@@ -1,3 +0,0 @@
-app.controller("contentCreateController", function ($scope, $routeParams,contentTypeFactory) {	
-	$scope.allowedTypes  = contentTypeFactory.allowedTypes($scope.currentNode.id);	
-});
