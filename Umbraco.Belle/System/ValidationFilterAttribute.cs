da04d2d6fc758e7c8d877e5e9f457962748f3c5b
Shannon Deminick <sdeminick@gmail.com>
2013-05-27 14:23:28 +1000

    updated validation files

---
+++ Umbraco.Belle/System/ValidationFilterAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Umbraco.Belle.System.Mvc
+{
+    /// <summary>
+    /// An action filter used to do basic validation against the model and return a result
+    /// straight away if it fails.
+    /// </summary>
+    internal class ValidationFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var modelState = actionContext.ModelState;
+
+            if (!modelState.IsValid)
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, modelState);
+            }
+
+        }
+    }
+
+}
