e428d4eaeb52b3b549a906ea7a19abebbd8310a2
Thor Madsen-Holm <thormadsenholm@gmail.com>
2013-05-29 21:17:39 +0200

    Added seperate stylesheet for custom datepicker styling. Added dependency for the stylesheet in datepicker.controller.js Comments added in controller and stylesheet Tidy up in Editor.html Added hack in bootstrap.datepicker.js to ensure correct placement of datepicker window

--- Umbraco.Belle.Client/src/views/propertyeditors/umbraco/datepicker/datepicker.controller.js
+++ Umbraco.Belle.Client/src/views/propertyeditors/umbraco/datepicker/datepicker.controller.js
@@ -1,17 +1,23 @@
 angular.module("umbraco").controller("Umbraco.Editors.DatepickerController", function ($rootScope, $scope, notifications, $timeout) {
     require(
         [
-            'views/propertyeditors/umbraco/datepicker/bootstrap.datepicker.js'
+            'views/propertyeditors/umbraco/datepicker/bootstrap.datepicker.js',
+            'css!/belle/views/propertyeditors/umbraco/datepicker/bootstrap.datepicker.css'
         ],
         function () {
-        	var pickerId = $scope.model.alias;
+            //The Datepicker js and css files are available and all components are ready to use.
 
-        	$("#" + pickerId).datepicker({
-        		format: "dd/mm/yyyy",
-        		autoclose: true
-        	}).on("changeDate", function (e) {
-				$scope.model.value = e.date;
-        	});
+            // Get the id of the datepicker button that was clicked
+            var pickerId = $scope.model.alias;
+
+            // Open the datepicker and add a changeDate eventlistener
+            $("#" + pickerId).datepicker({
+                format: "dd/mm/yyyy",
+                autoclose: true
+            }).on("changeDate", function (e) {
+                // When a date is clicked the date is stored in model.value
+                $scope.model.value = e.date;
+            });
         }
     );
 });
