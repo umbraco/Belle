cbd379eef383d060fdd47d05107a287511b6cad4
Shannon Deminick <sdeminick@gmail.com>
2013-06-05 18:44:16 +1000

    removed old val-regex directive, this has been superceded by another one in the v7 source

--- Umbraco.Belle.Client/src/common/directives/directives.js
+++ Umbraco.Belle.Client/src/common/directives/directives.js
@@ -1,32 +1,4 @@
 angular.module('umbraco.directives', [])
-.directive('val-regex', function () {
-
-        /// <summary>
-        ///     A custom directive to allow for matching a value against a regex string.
-        ///     NOTE: there's already an ng-pattern but this requires that a regex expression is set, not a regex string
-        ///</summary>
-
-        return {
-            require: 'ngModel',
-            link: function (scope, elm, attrs, ctrl) {
-
-                var regex = new RegExp(scope.$eval(attrs.valRegex));
-
-                ctrl.$parsers.unshift(function (viewValue) {
-                    if (regex.test(viewValue)) {
-                        // it is valid
-                        ctrl.$setValidity('val-regex', true);
-                        return viewValue;
-                    }
-                    else {
-                        // it is invalid, return undefined (no model update)
-                        ctrl.$setValidity('val-regex', false);
-                        return undefined;
-                    }
-                });
-            }
-        };
-    })
 
 .directive('appVersion', ['version', function (version) {
     return function (scope, elm, attrs) {
